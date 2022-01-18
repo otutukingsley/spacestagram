41b9bd22bf448dbfdc88629212c1628b2a2750bf
otutukingsley <chineduotutu3@gmail.com>
2022-01-18 03:45:25 -0800

    nav link fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,22 @@ import { Nav } from './componentStyles/NavbarStyles'
 const Navbar = () => {
   return (
     <Nav>
-      <div className="wrapper">
-        <h1 className="nav-heading">Spacestagram</h1>
-        <h5 className="nav-text">brought to you by NASA's image API</h5>
-      </div>
+      <nav className="nav-bar">
+        <div className="wrapper nav-wrapper">
+          <div className="headings">
+            <h1 className="nav-heading">Spacestagram</h1>
+            <h5 className="nav-text">brought to you by NASA's image API</h5>
+          </div>
+          <a
+            href="https://github.com/otutukingsley/spacestagram"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-link"
+          >
+            View Project Repo
+          </a>
+        </div>
+      </nav>
     </Nav>
   )
 }
--- src/components/componentStyles/NavbarStyles.js
+++ src/components/componentStyles/NavbarStyles.js
@@ -1,23 +1,53 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Nav = styled.header`
-position: sticky;
-top: 0;
-left: 0;
-right: 0;
-z-index: 10;
-box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
-background-color: ${({ theme }) => theme.colors.skyBlue};
-color: ${({ theme }) => theme.colors.white};
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  background-color: ${({ theme }) => theme.colors.skyBlue};
+  color: ${({ theme }) => theme.colors.white};
 
- .nav-heading{
-  font-weight: bold;
-  font-size: clamp(1rem, 2.5vw, 2rem);
-  margin-bottom: 0.5rem;
- }
+  .nav-bar {
+    width: 100%;
+    display: block;
+  }
 
- .nav-text{
-  font-weight: lighter;
-  font-size: clamp(.8rem, 2.5vw, 1rem);
- }
+  .nav-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .nav-link {
+    color: white;
+    font-size: clamp(1rem, 2.5vw, 1.4rem);
+    font-weight: bold;
+  }
+
+  .nav-heading {
+    font-weight: bold;
+    font-size: clamp(1rem, 2.5vw, 2rem);
+    margin-bottom: 0.5rem;
+  }
+
+  .nav-text {
+    font-weight: lighter;
+    font-size: clamp(0.8rem, 2.5vw, 1rem);
+  }
+
+  @media (max-width: 519px) {
+    .nav-wrapper {
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+    }
+
+    .nav-wrapper > * + * {
+        margin: 1rem 0 0 0 ;
+    }
+  }
 `
